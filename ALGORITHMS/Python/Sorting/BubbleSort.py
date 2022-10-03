12b72a60a563ffe4ce3460376dd91bd7f2cb8f61
Dhruv-Dugar <dhruvdugar02@gmail.com>
2022-10-03 22:10:07 +0530

    added comments

--- ALGORITHMS/Python/Sorting/BubbleSort.py
+++ ALGORITHMS/Python/Sorting/BubbleSort.py
@@ -1,10 +1,17 @@
+# working on bubble sort in Python
+# make a function in which we pass the array, compute the length
+
+
 def bubbleSort(arr):
     n = len(arr)
+    # flag variable in which we check, if element is swapped or not
     swapped = False
     for i in range(n-1):
+        # iterating over the array twice, in order to sort only with the unsorted elements
         for j in range(0,n-i-1):
             if arr[j] > arr[j+1]:
                 swapped = True
+                # clever python way of swapping 2 variables
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     
     if not swapped:
@@ -20,3 +27,5 @@ bubbleSort(arr)
 print("Sorted array is:")
 for i in range(len(arr)):
     print(f"{arr[i]} ")
+
+# Dhruv Dugar, 2022
